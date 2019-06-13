187dd73ec6a4f4ba7895175f02fcdbfba59dccc0
Ammar <ammar@inexisconsulting.com>
2019-06-13 17:13:40 +0530

    Initial commit

---
+++ BethonysPieShop/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using BethonysPieShop.Models;
+using BethonysPieShop.Models.Context;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace BethonysPieShop
+{
+    public class Program
+    {
+        //1 Asp.net core app is like an console app. It has a Main method as entry point
+        public static void Main(string[] args)
+        {
+            //CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+
+            //getting access to dependency injection container inside the Main method via the Services collection
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;//getting all services in scope
+                try
+                {
+                    var context = services.GetRequiredService<AppDbContext>();//getting required context
+                    DbInitializer.Seed(context);
+                }
+                catch (Exception)
+                {
+                    //we could log this in a real-world situation
+                }
+            }
+
+            host.Run();
+        }
+
+        //2 creating the hosting environment behind the scenes
+        //this will create an internal web server(kestrel) and also the iis server
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args) //CreateDefaultBuilder() will create many defaults
+                .UseStartup<Startup>();
+    }
+}
---
+++ BethonysPieShop/Startup.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethonysPieShop.Models;
+using BethonysPieShop.Models.Context;
+using BethonysPieShop.Models.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BethonysPieShop
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        //aspnetcore automatically reads our configs(e.g appsetting.json) via program.cs-> CreateDefaultBuilder
+        //those configs are available in the app via IConfiguration
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        //3 configure build in and our own custom service here
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddTransient<IPieRepository, PieRepository>(); // AddTransient -> whenever someone ask for an instance of IPieRepository, provide a new MockPieRepository
+            services.AddTransient<IFeedbackRepository, FeedbackRepository>();
+            services.AddMvc(); //enabling MVC for this application
+        }
+
+        //4 setup the request pipeline
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            //configure middleware components here
+            //sequence of adding them is important
+            app.UseDeveloperExceptionPage(); //to get the yellow colour exception page
+            app.UseStatusCodePages(); //show the status of the responses
+            app.UseStaticFiles(); //support(search/get) for static files which are in wwwroot folder
+            app.UseAuthentication();
+            //app.UseMvcWithDefaultRoute();// this should be added after UseStaticFiles()
+
+            app.UseMvc(routes =>
+            {
+                //since Index action in Home controller is default, url will not show these segments
+                routes.MapRoute(
+                    name: "defaule",
+                    template: "{controller=Home}/{action=Index}/{id?}"
+                    );
+            });
+        }
+    }
+}
