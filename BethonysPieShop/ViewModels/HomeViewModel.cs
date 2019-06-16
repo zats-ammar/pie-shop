9ff11ae632b78751bf254c7710491fbdc6b3643b
Ammar <ammar@inexisconsulting.com>
2019-06-16 21:30:15 +0530

    list page

---
+++ BethonysPieShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethonysPieShop.Models;
+
+namespace BethonysPieShop.ViewModels
+{
+    public class HomeViewModel
+    {
+        public string Title { get; set; }
+        public List<Pie> Pies { get; set; }
+    }
+}
