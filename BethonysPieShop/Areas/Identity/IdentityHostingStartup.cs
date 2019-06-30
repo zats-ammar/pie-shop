48f4551e0e4df696be0ba977ead3a8f71384ed41
Ammar <ammar@inexisconsulting.com>
2019-06-30 21:59:24 +0530

    aspnet core identity integration

---
+++ BethonysPieShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using BethonysPieShop.Models.Context;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BethonysPieShop.Areas.Identity.IdentityHostingStartup))]
+namespace BethonysPieShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        //configuring identity
+        public void Configure(IWebHostBuilder builder)
+        {
+            //registering services for identity
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDefaultIdentity<IdentityUser>() //bring support for identity
+                .AddEntityFrameworkStores<AppDbContext>(); //specifies that our appdbcontext is used to store identity info 
+            });
+        }
+    }
+}
